68c7973122bbde929f46997f9129a9bd7bf6917e
Ta_Minh_Quan <taminhquan2309@gmail.com>
2025-03-16 14:12:50 +0700

    xoa 1 vai class

--- app/src/main/java/com/example/testthu/nhap.kt
+++
@@ -1,8 +0,0 @@
-package com.example.testthu
-
-class nhap{
-    fun main(args: Array<String>) {
-        var x: String="-232"
-        System.out.println(x.substring(1))
-    }
-}
--- app/src/main/java/com/example/testthu/them_roi_xoa1.kt
+++
@@ -1,4 +0,0 @@
-package com.example.testthu
-
-class them_roi_xoa1 {
-}
